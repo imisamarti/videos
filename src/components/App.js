31af4c26e06dbda34bacc92ca733a94715766408
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-06-03 16:11:23 -0400

    Create Video List component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 import config from './config';
 import youtube from '../apis/youtube';
 
 const KEY = config.access_key;
 
+
 class App extends React.Component{
 
-	onTermSubmit = (term) => {
-		youtube.get('/search',{
+	state = {videos:[]};
+
+	onTermSubmit = async (term) => {
+		const response = await youtube.get('/search',{
 			params:{
-				q:term,part:'snippet',
+				q:term,
+				part:'snippet',
 				type: 'video',
 				maxResults: 5,
 				key: KEY
 			}
 		});
+
+		this.setState({videos:response.data.items});
 	};
 
 	render(){
-		return <div className='ui container'><SearchBar onTermSubmit={this.onTermSubmit}/></div>
+		return (
+		<div className='ui container'><SearchBar onTermSubmit={this.onTermSubmit}/>
+		<VideoList videos={this.state.videos}/>
+		</div>
+		)
 	}
 }
 
