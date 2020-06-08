9d5f84f21d0a48685e1c895dbf3364969751f359
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-06-08 13:26:15 -0400

    Fix warning issues about alt-text in Video Item and warning about iFrame title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './VideoItem.css';
 
+//uses callback function "onVideoSelect"
+// to pass info to parent "VideoList"
 const VideoItem = ({video, onVideoSelect}) => {
 	return (
 		<div onClick={() => onVideoSelect(video)} className='video-item item'>
-		<img className='ui image' src={video.snippet.thumbnails.medium.url}/>
+		<img alt={video.snippet.title} className='ui image' src={video.snippet.thumbnails.medium.url}/>
 		<div className='content'><div className='header'>{video.snippet.title}</div></div>
 		</div>
 		)
